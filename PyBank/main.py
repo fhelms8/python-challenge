6637fd2567a93cfc6bcf2f0070254df1d661978d
Felecia Helms <85764497+fhelms8@users.noreply.github.com>
2021-07-18 10:05:22 -0500

    Completed PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,61 @@
-# PyBank Data
-import os
+# # PyBank Data
 import csv
+import statistics
 
-# #Set path for file 
-csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
-
-#Open the CSV File
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
-    header = next(csvreader)
-
-#Set variables
-tot_months=1
-net_total=0
+# #Set variables
+tot_months= 0
+tot_PL= 0.0
 ch_PL = []
 increase_prof = ["",0]
 decrease_prof = ["",0]
-profit = []
+prev_mon_PL= 0
+result = []
 
-# for row in csvreader:
-#         tot_months.append(row[0])
-#         net_total.append(int(row[1]))
-# for p in range(len(profit)-1):
-#         ch_PL.append(profit[p+1]-profit[p])
+# #Open the CSV File
+with open("Resources/budget_data.csv", 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
 
-for row in csvreader:
+    header = next(csvreader)
+
+    for row in csvreader:
+        # print(row)
+        # Add Total number of months
+        tot_months = tot_months + 1
+
+        #Add profit/loss
+        tot_PL = tot_PL + int(row[1])
+
+        #Calculate Changes in PL then average
+        if tot_months >1:
+            ch_PL.append(int(row[1]) - prev_mon_PL)
+
+        prev_mon_PL = int(row[1])
+
+        #Greatest increase in prof (date and amount)
+        if increase_prof[1] < int(row[1]):
+            increase_prof[0] = row[0]
+            increase_prof[1] = int(row[1])
+
+        #The greatest decrease in profile (date and amount)
+        if decrease_prof[1] > int(row[1]):
+            decrease_prof[0] = row[0]
+            decrease_prof[1] = int(row[1])
+
+        
+    result.append('Financial Analysis')
+    result.append('--------------------')
+    result.append(f'Total Months: {tot_months}')
+    result.append(f'Total Profit/Loss: $ {tot_PL}')
+    result.append(f'Average Change: $ {round(statistics.mean(ch_PL),2)}')
+    result.append(f'Greatest Increase PL:  {increase_prof[0]}, $ {increase_prof[1]} ')
+    result.append(f'Greatest Decrease PL:  {decrease_prof[0]}, $ {decrease_prof[1]} ')
     
-
-#date = column 0
-#PL = column 1
-#Total # of months in data set; net total of PL over data set
-    # for row in csvreader:
-    #     tot_months = tot_months + 1
-    #     net_total = net_total + int(row[1])
-
-#Changes in PL over data set then average
+    #Printing to Python console
+    for item in result:
+        print(item)
+   
+    #Print results to Anaylsis txt: 
+    with open("Analysis/results.txt", 'w') as text_file:
+        for item in result:
+            text_file.write(item + "\n")
     
