3378510f6087c68472ac3b495191b97b29d6a8d0
Felecia Helms <85764497+fhelms8@users.noreply.github.com>
2021-07-19 21:34:15 -0500

    finished PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,70 +3,80 @@ import csv
 
 #Set Variables
 tot_votes= 0
-candidate = []
-perc_votes = 0.0
+candidate_choices = []
 candidates_votes = {}
-candidate_perc = {}
 winner = 0
 result = ""
+name_list = []
+count_list = 0
 
-# #Open the CSV File
 with open("Resources/election_data.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+
+# # #Open the CSV File
+# with open("Resources/election_data.csv", 'r') as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter = ",")
 
     header = next(csvreader)
 
     for row in csvreader:
-        
-        # Add Total number of votes
-        tot_votes = tot_votes + 1
-        
-        #Add list of candidates
-        candidate = row[2]
+        name_list.append(row[2])
 
-        #Calculate votes per candidate 
-        if candidate in candidates_votes:
-            candidates_votes[candidate] = candidates_votes[candidate] + 1 
-        else:
-            candidates_votes[candidate] = 1
-        print(candidates_votes)
+        # Add Total number of votes (*Correct*)
+        tot_votes = tot_votes + 1      
         
-        #Percentage format {:%}
-        def percent(x):
-            num = x/tot_votes
-            percentage = "{:.2%}".format(num)
-            print(percentage)
-
-        
-
+        #Add list of candidates (row 2)
     
-    #     prev_mon_PL = int(row[1])
+        candidate_choices.append(row[2])
 
-    #     #Greatest increase in prof (date and amount)
-    #     if increase_prof[1] < int(row[1]):
-    #         increase_prof[0] = row[0]
-    #         increase_prof[1] = int(row[1])
+    for candidate in set(candidate_choices):
+        name_list.append(candidate)
 
-    #     #The greatest decrease in profile (date and amount)
-    #     if decrease_prof[1] > int(row[1]):
-    #         decrease_prof[0] = row[0]
-    #         decrease_prof[1] = int(row[1])
+    Tooley = (candidate_choices.count)("O'Tooley")
+    Khan = (candidate_choices.count)("Khan")
+    Correy = (candidate_choices.count)("Correy")
+    Li = (candidate_choices.count)("Li")
+    
 
-        
-    # result.append('Election Results')
-    # result.append('--------------------')
-    # result.append(f'Total Votes: {tot_votes}')
-    # result.append('--------------------')
-    # result.append(f'candidates_votes: $ {tot_PL}')
-    # result.append(f'Average Change: $ {round(statistics.mean(ch_PL),2)}')
-    # result.append(f'Greatest Increase PL:  {increase_prof[0]}, $ {increase_prof[1]} ')
-    # result.append(f'Greatest Decrease PL:  {decrease_prof[0]}, $ {decrease_prof[1]} ')
+    #Set percentage functions
+    Tooley_percentage = (Tooley/tot_votes) * 100
+    Khan_percentage = (Khan/tot_votes) * 100
+    Correy_percentage = (Correy/tot_votes) * 100
+    Li_percentage = (Li/tot_votes) * 100
+
+    Tooley_percentage = "{0:.2f}%".format(Tooley_percentage)
+    Khan_percentage = "{0:.2f}%".format(Khan_percentage)
+    Correy_percentage = "{0:.2f}%".format(Correy_percentage)
+    Li_percentage = "{0:.2f}%".format(Li_percentage)
+
+
+    #Calcuate winner 
+    if Tooley > Khan > Correy > Li:
+       Winner = "O'Tooley"
+    elif Khan > Correy > Li > Tooley:
+       Winner = "Khan"
+    elif Correy > Khan > Li > Tooley:
+       Winner = "Correy"
+    elif Li > Khan > Correy > Tooley:
+       Winner = "Li"
+    
+    
+
+    print("Election Results")
+    print('-----------------')
+    print(f'Total Votes: {tot_votes}')
+    print('------------------')
+    print(f"Khan: {Khan_percentage}% ({Khan})")
+    print(f"Correy: {Correy_percentage}% ({Correy})")
+    print(f"Li: {Li_percentage}% ({Li})")
+    print(f"O'Tooley: {Tooley_percentage}% ({Tooley})")
+    print(f'Winner: {Winner}')
     
     #Printing to Python console
     for item in result:
         print(item)
    
-    # #Print results to Anaylsis txt: 
-    # with open("Analysis/results.txt", 'w') as text_file:
-    #     for item in result:
-    #         text_file.write(item + "\n")
+    #Print results to Anaylsis txt: 
+    with open("Analysis/results.txt", 'w') as text_file:
+        for item in result:
+            text_file.write(item + "\n")
